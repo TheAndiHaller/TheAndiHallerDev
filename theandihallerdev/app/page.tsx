52a407ff281831323759b12fa66e2800429199b3
Andi <andi_haller@hotmail.com>
2024-11-06 09:43:56 -0300

    Deployable Version

--- theandihallerdev/app/page.tsx
+++ theandihallerdev/app/page.tsx
@@ -1,9 +1,5 @@
-import LatestProjects from "@/app/ui/latestProjects";
 import Bio from "@/app/ui/bio";
 import WorkingOn from "./ui/working-on";
-import LatestBlog from "./ui/latestBlog";
-import BlogCardContainer from "@/app/ui/BlogCardContainer";
-import ProjectCardContainer from "@/app/ui/ProjectCardContainer";
 
 export default function Home() {
   return (
@@ -11,12 +7,6 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-[600px] mx-auto">
         <Bio />
         <WorkingOn />
-        <ProjectCardContainer >
-          <LatestProjects />
-        </ProjectCardContainer>
-        <BlogCardContainer>
-          <LatestBlog />
-        </BlogCardContainer>
       </main>
     </div>
   );
